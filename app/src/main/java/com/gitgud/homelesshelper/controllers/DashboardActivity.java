1337a06b679b366c6cf1f26b814a07882fe02236
Collin Avidano <collin.avidano@gmail.com>
2018-02-14 00:14:19 -0500

    V0.1.0 - Intial Commit

---
+++ app/src/main/java/com/gitgud/homelesshelper/controllers/DashboardActivity.java
@@ -0,0 +1,22 @@
+package com.gitgud.homelesshelper.controllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gitgud.homelesshelper.R;
+
+/**
+ * Created by collin on 2/13/18.
+ */
+
+public class DashboardActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+    }
+
+}
---
+++ app/src/main/java/com/gitgud/homelesshelper/controllers/WelcomeActivity.java
@@ -0,0 +1,30 @@
+package com.gitgud.homelesshelper.controllers;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.gitgud.homelesshelper.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        loginButton = (Button) findViewById(R.id.buttonLogin);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+}
