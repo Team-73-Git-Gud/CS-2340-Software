8b9634914572bea070559ebc614d1b4d50dec0c7
Collin Avidano <collin.avidano@gmail.com>
2018-02-21 16:53:14 -0500

    V1.1.0 - Demo branch is actually going to be merged.

--- app/src/main/java/com/gitgud/homelesshelper/controllers/DashboardActivity.java
+++ app/src/main/java/com/gitgud/homelesshelper/controllers/DashboardActivity.java
@@ -1,7 +1,10 @@
 package com.gitgud.homelesshelper.controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.gitgud.homelesshelper.R;
 
@@ -10,10 +13,20 @@ import com.gitgud.homelesshelper.R;
  */
 
 public class DashboardActivity extends AppCompatActivity{
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        logoutButton = (Button) findViewById(R.id.buttonLogout);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
