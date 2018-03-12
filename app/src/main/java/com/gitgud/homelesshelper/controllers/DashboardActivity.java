dbb6876b3230db1e0422b373799130a4884cf57c
apexhydra <zachwaterssteam@yahoo.com>
2018-03-12 11:17:03 -0400

    added ability to put data into the database

--- app/src/main/java/com/gitgud/homelesshelper/controllers/DashboardActivity.java
+++ app/src/main/java/com/gitgud/homelesshelper/controllers/DashboardActivity.java
@@ -8,9 +8,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.firebase.client.Firebase;
 import com.gitgud.homelesshelper.R;
 import com.gitgud.homelesshelper.model.CSVParser;
+import com.gitgud.homelesshelper.model.Shelter;
 import com.gitgud.homelesshelper.model.User;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import java.util.Random;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by collin on 2/13/18.
@@ -23,8 +42,16 @@ public class DashboardActivity extends AppCompatActivity{
     private TextView type;
     private TextView name;
 
+    //Database Stuff
+    private Firebase mRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Database Stuff
+        Firebase.setAndroidContext(this);
+        mRef = new Firebase("https://cs-2340-software.firebaseio.com/");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
         type = (TextView) findViewById(R.id.textView3);
@@ -49,6 +76,7 @@ public class DashboardActivity extends AppCompatActivity{
         });
 
         stuff = new CSVParser(DashboardActivity.this);
-
+        Firebase mRefChild = mRef.child("Shelter");
+        mRefChild.setValue(Shelter.getShelterList());
     }
 }
